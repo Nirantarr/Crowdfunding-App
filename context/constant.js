3ea7497a97199af2583c34e695eb8ec50034b3d5
Nirantarr <nirantar.m844@gmail.com>
2023-04-17 21:46:32 +0530

    Update constant.js
    Corrected the directory for crowdfunding ABI

--- context/constant.js
+++ context/constant.js
@@ -1,4 +1,4 @@
-import CrowdFunding from './Crowdfunding.json';
+import CrowdFunding from './CrowdFunding.json';
 
 export const crowdFundingAddress="0x387978b37f42cBCd98CA46D53ffc2e6D8d582D6D";
 export const crowdFundingABI=CrowdFunding.abi;
